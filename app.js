6d0bdb1c896813211f87653061c1cd4d7501936f
Lishan <lishan.low11@gmail.com>
2023-05-16 11:11:19 +0100

    added basic html, css and js

---
+++ app.js
@@ -0,0 +1,3 @@
+const misfortunes = ['A bird will poop on you', 'You will step in dog poo', 
+'You will forget the housekeys (again)', 'Slip on a banana peel', 
+'You will be surrounded by thirteen black cats under a ladder (Don/t ask how)'];
